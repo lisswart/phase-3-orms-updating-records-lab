e70c1e85c7c466d974d332dd98726527b1627145
lisswart <arnoudlisa@gmail.com>
2021-07-22 14:48:15 -0400

    done

--- lib/student.rb
+++ lib/student.rb
@@ -31,26 +31,34 @@ class Student
   end
 
   def save
+    # if called on an object that is already persisted
     if self.id
+      # updates a record
       self.update
+      # otherwise,
     else
+      # saves the attributes of an instance of the Student class to the database 
       sql = <<-SQL
         INSERT INTO students (name, grade)
         VALUES (?, ?)
       SQL
-
       DB[:conn].execute(sql, self.name, self.grade)
 
+      # and then sets the given students 'id' attribute
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
 
   def self.create(name, grade)
+    # creates a student with two attributes, name and grade,
     student = Student.new(name, grade)
+    # and saves it into the students table
     student.save
   end
 
+  # the following method is used in another class method .find_by_name(name)
   def self.new_from_db(row)
+    # creates an instance with corresponding attribute values
     id = row[0]
     name = row[1]
     grade = row[2]
@@ -58,6 +66,7 @@ class Student
   end
 
   def self.find_by_name(name)
+    # returns an instance of student that matches the name from the DB
     sql = <<-SQL
       SELECT *
       FROM students
@@ -71,6 +80,7 @@ class Student
   end
 
   def update
+    # updates the record associated with a given instance
     sql = <<-SQL
       UPDATE students 
       SET name = ?, grade = ? 
